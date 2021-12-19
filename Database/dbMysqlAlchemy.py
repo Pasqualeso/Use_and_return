8915f62327c33442ec33bb1d262e78820456e262
Pasqualeso <86675688+Pasqualeso@users.noreply.github.com>
2021-12-19 21:15:25 +0100

    Aggiunte immagini

--- Database/dbMysqlAlchemy.py
+++ Database/dbMysqlAlchemy.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+import pymysql
 import secretsData
 
 conn = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(secretsData.dbUser, secretsData.dbPass, secretsData.dbHost,
