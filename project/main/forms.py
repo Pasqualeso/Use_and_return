1638f917ea344951d8d8ae9da1191871f13a070b
Pasqualeso <86675688+Pasqualeso@users.noreply.github.com>
2022-01-11 15:43:38 +0100

    Front-end modifica searchbox responsiva

--- project/main/forms.py
+++ project/main/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 
 class RegistrationFormRicerca(FlaskForm):
-    oggetto_ricerca = StringField("Inserisci oggetto)", validators=[DataRequired()])
+    oggetto_ricerca = StringField("Inserisci oggetto", validators=[DataRequired()])
     categoria_ricerca = SelectField(
         "Inserisci la categoria",
         choices=[("musica", "Musica"), ("telefonia", "Telefonia"), ("console e videogiochi", "Console e videogiochi"),
@@ -14,7 +14,7 @@ class RegistrationFormRicerca(FlaskForm):
                  ("video-maker", "Video-maker"), ("altro", "Altro")]
         , validators=[DataRequired()])
     regione_ricerca = SelectField(
-        "Inserisci la regione",
+        "Regione",
         choices=[("valle d'aosta", "Valle d'Aosta"), ("piemonte", "Piemonte"), ("liguria", "Liguria"),
                  ("lombardia", "Lombardia"), ("trentino-alto adige", "Trentino-Alto Adige"),
                  ("veneto", "Veneto"), ("friuli-venezia giulia", "Friuli-Venezia Giulia"),
@@ -25,7 +25,7 @@ class RegistrationFormRicerca(FlaskForm):
                  ("sicilia", "Sicilia"), ("sardegna", "Sardegna"), ("non specificare", "Non Specificare"), ]
 
         , validators=[DataRequired()])
-    data_inizio_noleggio_ricerca = DateField("Inserisci una data di inizio noleggio", validators=[DataRequired()])
-    data_fine_noleggio_ricerca = DateField("Inserisci una data di fine noleggio", validators=[DataRequired()])
+    data_inizio_noleggio_ricerca = DateField("Data di inizio noleggio", validators=[DataRequired()])
+    data_fine_noleggio_ricerca = DateField("Data di fine noleggio", validators=[DataRequired()])
 
     submit = SubmitField("Submit")
